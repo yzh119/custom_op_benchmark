ee17ac3d8dbf4f57152e0e47cce7dd98092fbb92
yzh119 <expye@outlook.com>
2019-05-10 21:43:15 +0000

    fix bug

--- part_csr.py
+++ part_csr.py
@@ -11,21 +11,24 @@ import torch as th
 
 
 def partition_csr(indptr, indices, eids, chunk_size=32):
+    device = indptr.device
     indptr = indptr.cpu()
     row = []
-    indptr_ = [0]
-    for i in range(indptr):
-        for j in range(indptr[i], indptr[i + 1] + chunk_size - 1, chunk_size):
+    indptr_ = []
+    for i in range(len(indptr) - 1):
+        for j in range(indptr[i], indptr[i + 1], chunk_size):
             row.append(i)
-            indptr_.append(min(j, indptr[i + 1])) 
+            indptr_.append(j)
 
-    row = th.tensor(row)
-    indptr_ = th.tensor(indptr_)
+    indptr_.append(indptr[-1])
+
+    row = th.tensor(row, device=device)
+    indptr_ = th.tensor(indptr_, device=device)
     return row, indptr_, indices, eids
 
 if __name__ == '__main__':
-    indptr = th.tensor([0, 4, 8, 10])
-    indices = th.tensor([1, 2, 3, 4, 2, 3, 4, 5, 0, 1])
-    eids = th.tensor([0, 1, 2, 3, 4, 5, 6])
-    row, indptr_ = partition_csr(indptr, indices, eids, chunk_size=2) 
+    indptr = th.tensor([0, 4, 8, 10]).cuda()
+    indices = th.tensor([1, 2, 3, 4, 2, 3, 4, 5, 0, 1]).cuda()
+    eids = th.tensor([0, 1, 2, 3, 4, 5, 6]).cuda()
+    row, indptr_, _, _ = partition_csr(indptr, indices, eids, chunk_size=2) 
     print(row, indptr_)
